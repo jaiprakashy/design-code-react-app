87d485e89bd82c0046dbc1ef46c8df7219c0d08a
Jaiprakash Yadav <yadavjaiprakash20@gmail.com>
2021-09-05 12:42:54 +0530

    Added cards

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,47 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from "../images/logo-designcode.svg"
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends React.Component {
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  constructor(props) {
+    super(props)
 
-Header.defaultProps = {
-  siteTitle: ``,
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+     window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true})
+    } else {
+      this.setState({hasScrolled: false})
+    }
+  }
+
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={logo} width="30" /></Link>
+          <Link to='/courses'>Courses</Link>
+          <Link to='/downloads'>Downloads</Link>
+          <Link to='/workshops'>Workshops</Link>
+          <Link to='/buy'><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
